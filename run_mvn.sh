fdc81b6271c4e7629ecf6b62c569c9058d60b900
Sridhar Sarnobat <ss401533@gmail.com>
2017-01-02 16:03:22 +0530

    Minor changes (untested)

--- run_mvn.sh
+++ run_mvn.sh
@@ -1,5 +1,5 @@
 mvn --quiet clean compile -Dcompiler.debug=true
 
-mvn --quiet exec:java -Dexec.mainClass="JavaVariableFlow2"
+mvn --quiet exec:java -Dexec.mainClass="JavaVariableFlow" | sort | uniq
 
 # cat out.csv  | sh ~/github/java_callgraph_csv/csv2d3.sh
